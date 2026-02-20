6ebcbf99e19cd542a5caa567fd8180175eac186a
Максим Павлів <maksimpavliv@MacBook-Pro-Maksim.local>
2026-02-20 12:00:58 +0200

    added data layer

---
+++ infra/data.tf
@@ -0,0 +1,35 @@
+resource "aws_db_subnet_group" "default" {
+  name       = "main-db-subnet-group"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+}
+
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+
+  vpc_security_group_ids = [aws_security_group.mysql_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+}
+
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "main-redis-subnet-group"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "wp-redis"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+
+  subnet_group_name  = aws_elasticache_subnet_group.default.name
+  security_group_ids = [aws_security_group.redis_sg.id]
+}
